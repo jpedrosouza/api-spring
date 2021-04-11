60c696475878ceea48d24509f6d5d1008e01a801
joaopedro9830 <jsouza.1650@gmail.com>
2021-04-11 05:53:30 -0300

    add update and delete methods from product

--- src/main/java/com/study/apispring/controllers/ProductController.java
+++ src/main/java/com/study/apispring/controllers/ProductController.java
@@ -4,9 +4,13 @@ import com.study.apispring.models.ProductModel;
 import com.study.apispring.repositories.ProductRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,5 +39,27 @@ public class ProductController {
     @GetMapping("/products")
     public @ResponseBody Iterable<ProductModel> getAllProducts() {
         return productRepository.findAll();
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<ProductModel> updateProduct(@PathVariable("id") Integer id,
+            @RequestBody ProductModel productModel) {
+
+        return productRepository.findById(id).map(record -> {
+            record.setName(productModel.getName());
+            record.setDescription(productModel.getDescription());
+            record.setPrice(productModel.getPrice());
+            record.setQuantity(productModel.getQuantity());
+            ProductModel updated = productRepository.save(record);
+            return ResponseEntity.ok().body(updated);
+        }).orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping("/delete")
+    public @ResponseBody String removeProduct(@RequestParam Integer id) {
+        productRepository.deleteById(id);
+
+        return "Deleted!";
     }
 }
